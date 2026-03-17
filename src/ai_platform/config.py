c85ccc5d67efded3e838161f1d725cbcf171550a
Ofri Serchy <ofri@Ofris-MacBook-Pro.local>
2026-03-17 16:48:08 +0200

    reconfigure that config file for anthropic api

--- src/ai_platform/config.py
+++ src/ai_platform/config.py
@@ -1,16 +1,31 @@
 from pathlib import Path
-from pydantic_settings import BaseSettings, YamlConfigSettingsSource
+from pydantic_settings import BaseSettings, YamlConfigSettingsSource, SettingsConfigDict, DotEnvSettingsSource
+from pydantic import BaseModel
 
 base_dir = Path(__file__).parent.parent.parent
 
+
+class AnthropicSettings(BaseModel):
+    api_key: str
+    model: str = "claude-sonnet-4-6"
+    max_tokens: int = 1024
+    
 #creating setting validator
 class Settings(BaseSettings):
+        model_config = SettingsConfigDict(
+            env_file=base_dir / ".env",
+            env_file_encoding= "utf-8",
+            env_nested_delimiter="__"
+
+        )
+        
         log_level: str = 'info'
         api_url: str
         api_timeout: int
         max_retries: int
         backoff_multiplier: int
-
+        anthropic:AnthropicSettings
+        
         @classmethod
         def settings_customise_sources(
             cls,
@@ -21,7 +36,8 @@ class Settings(BaseSettings):
             file_secret_settings, 
         ) :  
             return(
-            env_settings,           
+            env_settings,
+            DotEnvSettingsSource(settings_cls, (base_dir / ".env")),
             YamlConfigSettingsSource(settings_cls, (base_dir / "config.yaml"))
         )
-
+        
